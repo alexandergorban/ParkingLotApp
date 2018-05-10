449d4d3bfc5bb5632b2f476cd0e11aff1af1ab83
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-05-10 12:31:21 +0300

    Updated Car class #1

--- ParkingLotApp/Entities/Car.cs
+++ ParkingLotApp/Entities/Car.cs
@@ -8,8 +8,34 @@ namespace ParkingLotApp.Entities
 {
     class Car
     {
-        public uint Id { get; set; }
-        public float Balance { get; set; }
-        public CarType Type { get; set; }
+        public uint Id { get; private set; }
+        public decimal Balance { get; private set; }
+
+
+        private readonly CarType type;
+
+        public CarType GetType()
+        {
+            return type;
+        }
+
+        public Car(uint id, CarType type, decimal balance = 0)
+        {
+            Id = id;
+            Balance = balance;
+            type = type;
+        }
+
+        public decimal IncreaseBalance(decimal value)
+        {
+            Balance += value;
+            return Balance;
+        }
+
+        public decimal DecreaseBalance(decimal value)
+        {
+            Balance += value;
+            return Balance;
+        }
     }
 }
