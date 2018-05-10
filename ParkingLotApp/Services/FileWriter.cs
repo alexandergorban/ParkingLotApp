34bdfa4d63133c5d6eb67b3a53dd6632625c1953
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-05-10 16:43:26 +0300

    Added FileWriter for logging into file (fixes #8)

---
+++ ParkingLotApp/Services/FileWriter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingLotApp.Entities;
+
+namespace ParkingLotApp.Services
+{
+    static class FileWriter
+    {
+        // Set interval for logging (1 min)
+        private static TimeSpan interval = new TimeSpan(0, 1, 0);
+
+        public static void LogTransactionToFile(object obj)
+        {
+            var lastTransactionsForWrite =
+                Settings.Parking.Transactions.Where<Transaction>(t => DateTime.Now - t.GetTime() < interval);
+
+            using (StreamWriter sw = new StreamWriter("transactions.log", true, System.Text.Encoding.Default))
+            {
+                decimal accumulator = 0;
+                foreach (var transaction in lastTransactionsForWrite)
+                {
+                    accumulator += transaction.GetWithdrawMoney();
+                }
+
+                sw.WriteLine("DateTime: {0} Transaction Amount: {1}", DateTime.Now, accumulator);
+            }
+
+        }
+    }
+}
--- ParkingLotApp/Settings.cs
+++ ParkingLotApp/Settings.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using ParkingLotApp.Entities;
 
@@ -9,9 +10,31 @@ namespace ParkingLotApp
 {
     static class Settings
     {
+        private static Timer withdrawMoneyInterval;
+        private static Timer logToFile;
+
         public static DateTime Timeout { get; set; }
-        public static Dictionary<CarType, decimal> Dictionary { get; set; }
-        public static UInt16 ParkingSpace { get; set; }
-        public static decimal Fine { get; set; }
+
+        //Initialize prices for Parking Lot by Car Type
+        public static Dictionary<CarType, decimal> Dictionary { get; set; } = new Dictionary<CarType, decimal>()
+        {
+            { CarType.Motorcycle, 2 },
+            { CarType.Passenger, 5},
+            { CarType.Truck, 10},
+            { CarType.Bus, 12}
+        };
+
+        //Initialize Parking Size
+        public static UInt16 ParkingSpace { get; set; } = 100;
+
+        //Coefficient of Penalty
+        public static double Fine { get; set; } = 0.20;
+
+        public static Parking Parking { get; set; } = Parking.Instance;
+
+        static Settings()
+        {
+            
+        }
     }
 }
